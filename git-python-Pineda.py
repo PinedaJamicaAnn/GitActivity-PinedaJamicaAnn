8d05a6a3b5cf69e182da77fbea14f271f22e1dc9
PinedaJamicaAnn <jamicaannpineda@gmail.com>
2022-03-10 17:27:11 +0800

    My Educational Background

--- git-python-Pineda.py
+++ git-python-Pineda.py
@@ -1,3 +1,6 @@
 print ("Jamica Ann R. Pineda");
 print ("Brgy Bananahaw Santa Cruz Marinduque");
 print ("November 28 2001");
+print ("Marinduque State College");
+print ("BS Information Technology");
+print ("2nd Year, Section B");
